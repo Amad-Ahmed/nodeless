66ea98822ba0f80d7eb90d68c02a7fbbe175127a
Ray Deck <ray@raydeck.com>
2022-10-27 19:57:51 -0400

    feat: more output

--- netlify-functions/src/functions/hello.ts
+++ netlify-functions/src/functions/hello.ts
@@ -2,6 +2,12 @@ import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
   // your server-side functionality
-  return { statusCode: 200, body: "Hello World" };
+
+  const output = {
+    event,
+    body: event.body,
+    parsed: event.body && JSON.parse(event.body),
+  };
+  return { statusCode: 200, body: "<pre>" + JSON.stringify(output) + "</pre>" };
 };
 export { handler };
