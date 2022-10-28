25cd6920267aac8c2d7c7988c6d9128cfbc460b9
Ray Deck <ray@raydeck.com>
2022-10-28 17:57:42 -0400

    feat: update with cbor

--- netlify-functions/src/functions/hello.ts
+++ netlify-functions/src/functions/hello.ts
@@ -1,15 +1,19 @@
 import { Handler } from "@netlify/functions";
-
+import { decode } from "cbor";
 const handler: Handler = async (event, context) => {
   // your server-side functionality
-
+  const parsed = event.body && JSON.parse(event.body);
   const output = {
     event,
     body: event.body,
-    parsed: event.body && JSON.parse(event.body),
+    parsed,
   };
   console.log("I got a first request, how about that!!!!");
-  console.log(JSON.stringify(output, null, 2));
+  const { data } = parsed[0];
+  console.log("My data is ", data);
+  const parsedData = decode(data);
+  console.log("My parsed data is ", parsedData);
+  //  console.log(JSON.stringify(output, null, 2));
   return {
     statusCode: 200,
     body: "Booyah",
