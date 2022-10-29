3e89dce2b699f2803db8732b52f30c0668dcffbf
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-10-29 16:58:10 -0400

    feat: tested with big number

--- Oracle/scripts/requests.ts
+++ Oracle/scripts/requests.ts
@@ -7,7 +7,7 @@ async function main() {
 
     const Request = await ethers.getContractFactory("Request");
     const request = await Request.attach("0xba318bC26d86E8f67f98eBC632aC88BB05275A73");
-    const request_txn = await request.requestPrice("BTC/USD",11 , -16, 1010, ["this is a test", "array of strings"]);
+    const request_txn = await request.requestPrice("BTC/USD",11 , -1600000000, 10100001010101000n, ["this is a test", "array of strings"]);
     const request_txn_receipt = await request_txn.wait();
     console.log(`Request txn receipt: ${request_txn_receipt}`);
 }
