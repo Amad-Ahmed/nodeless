33cb2c162f6789a7a2faba96e1b9e321968a2e1e
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-11-03 20:36:41 -0400

    fix: changed stock symbol

--- Oracle/scripts/getPrice.ts
+++ Oracle/scripts/getPrice.ts
@@ -8,7 +8,7 @@ async function main() {
     const Request = await ethers.getContractFactory("Request");
     const request = await Request.attach("0x9bb18d9347cB7525787E0488F630dE93376D6e37");
 
-    const result = await request.getPriceandBlock("VOO");
+    const result = await request.getPriceandBlock("AMZN");
     console.log(`result: ${JSON.stringify(result, null, 2)}`);
 }
 
