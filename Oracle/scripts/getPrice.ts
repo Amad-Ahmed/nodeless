c04c8cc52b373baa5b7cc8b8674f35df164b37a8
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-11-03 20:46:05 -0400

    fix: changed symbol to BBBY

--- Oracle/scripts/getPrice.ts
+++ Oracle/scripts/getPrice.ts
@@ -6,7 +6,7 @@ import { ethers } from "hardhat";
 async function main() {
 
     const Request = await ethers.getContractFactory("Request");
-    const request = await Request.attach("0x9bb18d9347cB7525787E0488F630dE93376D6e37");
+    const request = await Request.attach("0x757822fBc77401E8D16bdC18E0D59327935De38f");
 
     const result = await request.getPriceandBlock("AMZN");
     console.log(`result: ${JSON.stringify(result, null, 2)}`);
--- Oracle/scripts/requests.ts
+++ Oracle/scripts/requests.ts
@@ -6,10 +6,10 @@ import { ethers } from "hardhat";
 async function main() {
 
     const Request = await ethers.getContractFactory("Request");
-    const request = await Request.attach("0x9bb18d9347cB7525787E0488F630dE93376D6e37");
+    const request = await Request.attach("0x757822fBc77401E8D16bdC18E0D59327935De38f");
     // const test_id = await request.callStatic.requestPrice("QQQ",11 , -1600000000000123n, 10100001010101000n, ["this is a test", "array of strings"]);
     // console.log(`test_id: ${JSON.stringify(test_id, null, 2)}`);
-    const request_txn = await request.requestPrice("AMZN",11 , -1600000000000123n, 10100001010101000n, ["this is a test", "array of strings"]);
+    const request_txn = await request.requestPrice("BBBY",11 , -1600000000000123n, 10100001010101000n, ["this is a test", "array of strings"]);
     console.log(`Request deployed to ${JSON.stringify(request_txn, null, 2)}`);
     const request_txn_receipt = await request_txn.wait();
     console.log(`Request txn receipt: ${JSON.stringify(request_txn_receipt,null, 2)}`);
