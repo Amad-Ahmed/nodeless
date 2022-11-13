dce6acd22e1f74d9b0dbd38c713a5cc87bca597c
Ray Deck <ray@raydeck.com>
2022-11-13 11:10:40 -0500

    feat: adding polish to the app basics

--- app/src/Login.tsx
+++ app/src/Login.tsx
@@ -127,7 +127,7 @@ const Login: FC = () => {
                       onClick={loginWithWallet}
                       className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
-                      Sign In WIth Wallet
+                      Sign In With Wallet
                     </button>
                   </div>
 
