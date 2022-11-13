c0a5887ab038a2c9853ab107b3c16d67df4b5902
Ray Deck <ray@raydeck.com>
2022-11-13 08:14:59 -0500

    feat: updates to logo and signup experience

--- app/src/Signup.tsx
+++ app/src/Signup.tsx
@@ -28,14 +28,14 @@ const Login: FC = () => {
                   to="/"
                   className="font-medium text-blue-200 hover:text-blue-100"
                 >
-                  start your 14-day free trial
+                  Log in to your account
                 </Link>
               </p>
             </div>
 
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
               <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form className="space-y-6" action="#" method="POST">
+                <div className="space-y-6">
                   <div>
                     <label
                       htmlFor="email"
@@ -106,10 +106,10 @@ const Login: FC = () => {
                       onClick={submitForm}
                       className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
-                      Sign in
+                      Sign Up
                     </button>
                   </div>
-                </form>
+                </div>
 
                 <div className="mt-6">
                   <div className="relative">
@@ -128,7 +128,7 @@ const Login: FC = () => {
                       onClick={loginWithWallet}
                       className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
-                      Sign In WIth Wallet
+                      Sign Up with Wallet
                     </button>
                   </div>
 
