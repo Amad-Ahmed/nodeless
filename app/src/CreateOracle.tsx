da9cd5444f704da72b4c2703e3caf6a680227764
Ray Deck <ray@raydeck.com>
2022-11-13 17:49:09 -0500

    feat: put space between cancel and submit buttons

--- app/src/CreateOracle.tsx
+++ app/src/CreateOracle.tsx
@@ -183,8 +183,8 @@ const CreateOracle: FC<{
                         disabled={!isValid || !dirty || isSubmitting}
                         className={
                           !isValid || !dirty || isSubmitting
-                            ? "inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            : "inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            ? "mr-4 inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            : "mr-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         }
                       >
                         Save
